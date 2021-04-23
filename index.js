02e4fdcf388f07e9446b38ffa396c9284ce4a4ab
Ross Curry <ross_curry@yahoo.co.uk>
2021-04-23 20:30:16 +0200

    build: beginning server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+
+require('dotenv').config(); 
+
+const port = process.env.SV_PORT;
+const router = require('./router');
+const cors = require('cors')
+
+app.use(cors({options: '*'}))
+app.use(express.json())
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost/${port} 🔥`);
+})
