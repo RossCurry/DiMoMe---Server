bed1426489ea2916195ca16748fbf19f048448b4
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-27 20:47:58 +0200

    fix: remove console.logs

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const cors = require('cors');
 app.use(cors({
   // todo change to .env when netfli
   origin: 'https://dimome-v1.herokuapp.com',
-  // origin: '*',
   credentials: true,
 }))
 
-// app.use(express.static('public'))
 app.use(express.json({limit: '50mb'}));
 app.use(router);
 
--- validation/validationControl.js
+++ validation/validationControl.js
@@ -2,31 +2,34 @@ var atob = require('atob');
 
 const userValidate = (req, res, next) => {
   const user = req.body;
-  // todo add check that send res.send() if not true
   const { email, name, localSelector, localName, password} = user;
-  if (!email || !name || !localSelector || !localName || !password) res.send('an input is missing or wrongly spelt')
-  //this gets changed in the DB
-  const localType = localSelector;
-  console.log('email', email);
-  console.log('name -> ', name);
-  req.body = {
-    email: atob(email), 
-    name: atob(name), 
-    localType: atob(localType), 
-    localName: atob(localName),
-    password: atob(password)
+  if (!email || !name || !localSelector || !localName || !password) {
+    res.send('an input is missing or wrongly spelt')
+  } else {
+    const localType = localSelector;
+    req.body = {
+      email: atob(email), 
+      name: atob(name), 
+      localType: atob(localType), 
+      localName: atob(localName),
+      password: atob(password)
+    }
+    next();
   }
-  next();
 }
 
 const validateLogin = (req,res, next) => {
   const user = req.body;
   const { email, password} = user;
-  req.body = {
-    email: atob(email), 
-    password: atob(password)
-  }
-  next();
+  if (!email || !password) {
+    res.send('an input is missing or wrongly spelt')
+  } else {
+    req.body = {
+      email: atob(email), 
+      password: atob(password)
+    }
+    next();
+  } 
 }
 
 //TODO if things were going better we would be encrypting too Bcrypt
