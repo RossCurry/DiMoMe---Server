cf178d0203698b4a00648f6d3c3c8ff9bbdb0a10
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-27 20:13:12 +0200

    fix: add a check to the validation control

--- validation/validationControl.js
+++ validation/validationControl.js
@@ -2,9 +2,13 @@ var atob = require('atob');
 
 const userValidate = (req, res, next) => {
   const user = req.body;
+  // todo add check that send res.send() if not true
   const { email, name, localSelector, localName, password} = user;
+  if (!email || !name || !localSelector || !localName || !password) res.send('an input is missing or wrongly spelt')
   //this gets changed in the DB
   const localType = localSelector;
+  console.log('email', email);
+  console.log('name -> ', name);
   req.body = {
     email: atob(email), 
     name: atob(name), 
